0cb8ea2627d0a0501b5fe433eb1484614312d589
Mahyar Mahmoudi <mahmoudimahyar@gmail.com>
2025-07-16 14:52:27 -0700

    Fix AWS deployment build issues
    - Remove conflicting molecular libraries from npm dependencies
    - Keep molecular libraries as CDN-only (3Dmol, NGL, Molstar, Kekule)
    - Update Vite config to exclude CDN libraries from bundling
    - Add RDKit and Kekule to CDN scripts in HTML
    - Resolve 'molstar package entry' build error
    - Successful production build: 235KB total, 70KB gzipped

--- lipid_viewer/vite.config.js
+++ lipid_viewer/vite.config.js
@@ -13,16 +13,38 @@ export default defineConfig({
     // Optimize for AWS Amplify
     sourcemap: false,
     rollupOptions: {
+      external: [
+        // These libraries are loaded via CDN, don't bundle them
+        '3dmol',
+        'ngl',
+        'molstar',
+        'kekule'
+      ],
       output: {
         manualChunks: {
-          vendor: ['react', 'react-dom'],
-          molecular: ['3dmol', 'ngl', 'molstar', 'kekule', 'smiles-drawer']
+          vendor: ['react', 'react-dom', 'react-hot-toast'],
+          utils: ['smiles-drawer']
+        },
+        globals: {
+          // Map CDN libraries to their global variables
+          '3dmol': '$3Dmol',
+          'ngl': 'NGL',
+          'molstar': 'molstar',
+          'kekule': 'Kekule'
         }
       }
     }
   },
   optimizeDeps: {
-    include: ['rdkit-js']
+    include: ['smiles-drawer'],
+    exclude: [
+      // Exclude CDN libraries from optimization
+      '3dmol',
+      'ngl', 
+      'molstar',
+      'kekule',
+      'rdkit-js'
+    ]
   },
   // For SPA routing
   preview: {
